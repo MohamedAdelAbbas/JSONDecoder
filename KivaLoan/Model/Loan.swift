ae9990edfecdd77129df8ca6578491965786d226
Mohamed Adel <m.3del95@gmail.com>
2020-06-27 12:27:31 +0200

    Add Loan Model + Enhance Structure

---
+++ KivaLoan/Model/Loan.swift
@@ -0,0 +1,15 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Mohamed Adel on 6/27/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import Foundation
+struct Loan {
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+}
